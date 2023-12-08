5684141c65dd5b1c596e5a121df6059d738bb94d
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2023-12-08 23:42:47 +0100

    IOD-2 Add new functions to device and file inteface (#2)
    Allow to get position, seek position
    File can return vector of lines

--- src/file.cpp
+++ src/file.cpp
@@ -1,4 +1,6 @@
 #include "file.h"
+#include <text_processing/common.hpp>
+#include <text_processing/split.hpp>
 
 #include <unordered_map>
 
@@ -15,12 +17,29 @@ namespace IO{
 
         std::string File::read(){
             fseek(_ptr, 0, SEEK_END);
-            auto sz = ftell(_ptr);
-            fseek(_ptr, 0, SEEK_SET);
+            auto sz = pos();
+            seek(0);
             std::string buffer;
             buffer.resize(sz);
             auto read = fread(buffer.data(), sz, 1, _ptr);
             buffer.resize(read);
             return buffer;
+        }
+
+        std::vector<std::string> File::read_lines(){
+            return Text::to_string_vector(Text::split(read(), '\n'));
+        }
+
+        int64_t File::pos() const{
+            return ftell(_ptr);
+        }
+        int64_t File::seek(int64_t new_position){
+            if (fseek(_ptr, new_position, SEEK_SET) == 0)
+                return pos();
+            return -1;
+        }
+
+        bool File::is_sequential() const{
+            return true;
         }
 }
