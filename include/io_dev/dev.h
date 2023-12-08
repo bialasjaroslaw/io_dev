bc4357d49192d888da6f027bb775746c5e9bbeb3
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2023-12-08 23:38:56 +0100

    IOD-2 Add new functions to device and file inteface
    Allow to get position, seek position
    File can return vector of lines

--- include/io_dev/dev.h
+++ include/io_dev/dev.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <string>
+
 namespace IO{
     class Device{
         public:
             virtual ~Device() = 0;
+            virtual std::string read() = 0;
+            virtual int64_t pos() const = 0;
+            virtual int64_t seek(int64_t new_position) = 0;
+            virtual bool is_sequential() const = 0;
     };
 }
--- include/io_dev/file.h
+++ include/io_dev/file.h
@@ -14,11 +14,17 @@ namespace IO{
 
     class File: public Device{
         public:
-        File(const std::filesystem::path& path, OpenMode mode);
-        ~File() override;
-        std::string read();
+            File(const std::filesystem::path& path, OpenMode mode);
+            ~File() override;
+
+            std::string read() override;
+            int64_t pos() const override;
+            int64_t seek(int64_t new_position) override;
+            bool is_sequential() const override;
+            
+            std::vector<std::string> read_lines();
         private:
-        FILE* _ptr = nullptr;
-        std::filesystem::path _path;
+            FILE* _ptr = nullptr;
+            std::filesystem::path _path;
     };
 }
