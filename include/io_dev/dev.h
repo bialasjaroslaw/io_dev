648bd5b43118dea35da0bab75c053233bc383c8d
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2023-12-09 12:30:12 +0100

    Add new functionality

--- include/io_dev/dev.h
+++ include/io_dev/dev.h
@@ -1,14 +1,81 @@
 #pragma once
 
+#include <optional>
 #include <string>
+#include <span>
 
 namespace IO{
+    enum class OpenMode{
+        Unknown,
+        Read,
+        Write,
+        ReadBinary,
+        WriteBinary
+    };
+
+    enum class SeekMode{
+        Jump,
+        Skip,
+        End
+    };
+
+    constexpr int64_t ALL = -1;
+    constexpr int64_t UNKNOWN = -2;
+    constexpr int64_t INVALID = -3;
     class Device{
         public:
             virtual ~Device() = 0;
-            virtual std::string read() = 0;
+            virtual bool is_open() const = 0;
+            virtual OpenMode mode() const = 0;
+            virtual bool close() = 0;
+            virtual int64_t read_bytes(int64_t amount = ALL, char* buff = nullptr, int64_t buff_size = 0) = 0;
+            bool can_read() const{
+                return mode() == OpenMode::Read || mode() == OpenMode::ReadBinary;
+            }
+            bool can_write() const{
+                return mode() == OpenMode::Write || mode() == OpenMode::WriteBinary;
+            }
+            std::string read(int64_t amount = ALL){
+                std::string data;
+                if(!can_read())
+                    return data;
+                auto sz = size();
+                // Could be an issue if device does not return correct size or returns 0
+                if(sz > 0)
+                    data.resize(sz);
+                auto bytes_read = read_bytes(ALL, data.data(), data.size());
+                data.resize(bytes_read);
+                return data;
+            }
             virtual int64_t pos() const = 0;
-            virtual int64_t seek(int64_t new_position) = 0;
+            virtual int64_t size() const = 0;
+            int64_t available() const{
+                auto p = pos();
+                auto s = size();
+                if(p == UNKNOWN || s  == UNKNOWN || p > s)
+                    return UNKNOWN;
+                return std::max(int64_t{0}, s - p);
+            }
+            virtual int64_t seek(int64_t offset, SeekMode mode = SeekMode::Jump) = 0;
+            int64_t skip(int64_t offset){
+                return seek(offset, SeekMode::Skip);
+            }
+            virtual bool is_good() const = 0;
             virtual bool is_sequential() const = 0;
+
+            operator bool() const{
+                return is_good();
+            }
+
+            template <typename T>
+            std::optional<T> read_as(){
+                // Maybe check if mode is correct for data type?
+                char buff[sizeof(T)];
+                if(read_bytes(sizeof(T), buff, sizeof(T)) != sizeof(T))
+                    return std::nullopt;
+                T value{};
+                memcpy(&value, buff);
+                return value;
+            }
     };
 }
--- include/io_dev/file.h
+++ include/io_dev/file.h
@@ -7,24 +7,25 @@
 #include <vector>
 
 namespace IO{
-    enum class OpenMode{
-        Read,
-        Write
-    };
-
     class File: public Device{
         public:
             File(const std::filesystem::path& path, OpenMode mode);
             ~File() override;
+            bool is_open() const override;
+            OpenMode mode() const override;
+            bool close() override;
 
-            std::string read() override;
+            int64_t read_bytes(int64_t amount = ALL, char* buff = nullptr, int64_t buff_size = 0) override;
             int64_t pos() const override;
-            int64_t seek(int64_t new_position) override;
+            int64_t seek(int64_t offset, SeekMode mode = SeekMode::Jump) override;
             bool is_sequential() const override;
             
             std::vector<std::string> read_lines();
         private:
             FILE* _ptr = nullptr;
             std::filesystem::path _path;
+            int64_t _pos = 0;
+            int64_t _size = 0;
+            OpenMode _mode = OpenMode::Unknown;
     };
 }
