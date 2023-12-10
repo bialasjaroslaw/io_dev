02433eb95a3fa2135aaadb2bd99553533e0aacd3
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2023-12-10 15:46:51 +0100

    IOD-5 Fix developer warnings, add checks and sanitizers (#5)
    * IOD-5 Fix developer warnings, add checks and sanitizers
    * Disable any diagnostic for non GCC (temporary solution)

--- include/io_dev/dev.h
+++ include/io_dev/dev.h
@@ -3,6 +3,7 @@
 #include <optional>
 #include <string>
 #include <span>
+#include <limits>
 
 namespace IO{
     enum class OpenMode{
@@ -19,45 +20,45 @@ namespace IO{
         End
     };
 
-    constexpr int64_t ALL = -1;
-    constexpr int64_t UNKNOWN = -2;
-    constexpr int64_t INVALID = -3;
+    constexpr uint64_t ALL = std::numeric_limits<uint64_t>::max();
     class Device{
         public:
             virtual ~Device() = default;
             virtual bool is_open() const = 0;
             virtual OpenMode mode() const = 0;
             virtual bool close() = 0;
-            virtual int64_t read_bytes(int64_t amount = ALL, char* buff = nullptr, int64_t buff_size = 0) = 0;
+            virtual std::optional<uint64_t> read_bytes(uint64_t amount = ALL, char* buff = nullptr, uint64_t buff_size = 0) = 0;
             bool can_read() const{
                 return mode() == OpenMode::Read || mode() == OpenMode::ReadBinary;
             }
             bool can_write() const{
                 return mode() == OpenMode::Write || mode() == OpenMode::WriteBinary;
             }
-            std::string read(int64_t amount = ALL){
+            std::optional<std::string> read(uint64_t amount = ALL){
                 std::string data;
                 if(!can_read())
-                    return data;
-                auto sz = size();
+                    return std::nullopt;
+                auto size_bytes = size();
                 // Could be an issue if device does not return correct size or returns 0
-                if(sz > 0)
-                    data.resize(sz);
+                auto resize_to = size_bytes.value_or(amount);
+                data.resize(std::min(resize_to, amount));
                 auto bytes_read = read_bytes(amount, data.data(), data.size());
-                data.resize(bytes_read);
+                if(!bytes_read)
+                    return std::nullopt;
+                data.resize(bytes_read.value());
                 return data;
             }
-            virtual int64_t pos() const = 0;
-            virtual int64_t size() const = 0;
-            int64_t available() const{
+            virtual std::optional<uint64_t> pos() const = 0;
+            virtual std::optional<uint64_t> size() const = 0;
+            std::optional<uint64_t> available() const{
                 auto p = pos();
                 auto s = size();
-                if(p == UNKNOWN || s  == UNKNOWN || p > s)
-                    return UNKNOWN;
-                return std::max(int64_t{0}, s - p);
+                if(!p || !s || p.value() > s.value())
+                    return std::nullopt;
+                return std::max(uint64_t{0}, s.value() - p.value());
             }
-            virtual int64_t seek(int64_t offset, SeekMode mode = SeekMode::Jump) = 0;
-            int64_t skip(int64_t offset){
+            virtual std::optional<uint64_t> seek(uint64_t offset, SeekMode mode = SeekMode::Jump) = 0;
+            std::optional<uint64_t> skip(uint64_t offset){
                 return seek(offset, SeekMode::Skip);
             }
             virtual bool is_good() const = 0;
